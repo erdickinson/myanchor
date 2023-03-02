80c77c60a9dcc7b7d02d43c82afdcc8538876c29
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-01 22:32:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 import serial
 
@@ -6,24 +6,46 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 myanchor = Flask(__name__)
 
-@myanchor.route("/")
+@myanchor.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if request.form.get('direction') == 'forward':
+            ser.write(b'F')
+        elif request.form.get('direction') == 'reverse':
+            ser.write(b'R')
+        elif request.form.get('direction') == 'off':
+            ser.write(b'O')
+        elif request.form.get('speed') is not None:
+            speed = int(request.form.get('speed'))
+            ser.write(str(speed).encode())
+
     return """
     <html>
         <body>
             <h1>DC Motor Controller</h1>
-            <input type="range" min="0" max="255" value="0" class="slider" id="myRange" oninput="send()">
+            <form id="motor-form" method="POST">
+                <label>Direction:</label>
+                <input type="radio" name="direction" value="forward"> Forward
+                <input type="radio" name="direction" value="reverse"> Reverse
+                <input type="radio" name="direction" value="off"> Off
+                <br>
+                <label>Speed:</label>
+                <input type="range" min="0" max="255" value="0" class="slider" name="speed">
+            </form>
             <script>
-                function send() {
-                    var sliderValue = document.getElementById("myRange").value;
+                var form = document.getElementById("motor-form");
+                var slider = document.querySelector(".slider");
+                form.addEventListener("input", function() {
                     var xhttp = new XMLHttpRequest();
-                    xhttp.open("GET", "/send/" + sliderValue, true);
-                    xhttp.send();
-                }
+                    xhttp.open("POST", "/", true);
+                    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    xhttp.send(new FormData(form));
+                });
             </script>
         </body>
     </html>
     """
+
 
 @myanchor.route("/send/<int:value>")
 def send(value):
