6e4c6d87e33c3021e8d421d15225c6d80abf3305
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-02 14:41:49 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+def send_to_arduino(value):
+    checksum = sum(map(ord, value)) % 256
+    message = "{}{:02X}\n".format(value, checksum)
+    ser.write(message.encode())
+    print("Sent value: {}".format(value))
+
+myanchor = Flask(__name__)
+
+@myanchor.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('speed') is not None:
+            speed = int(request.form.get('speed'))
+            send_to_arduino("S{:03d}".format(speed))
+            print("Speed value: {}".format(speed))
+
+    return """
+    <html>
+        <body>
+            <h1>DC Motor Controller</h1>
+            <form id="motor-form" method="POST">
+                <label>Speed:</label>
+                <input type="range" min="0" max="255" value="0" class="slider" name="speed">
+            </form>
+            <script>
+                var form = document.getElementById("motor-form");
+                var slider = document.querySelector(".slider");
+                form.addEventListener("input", function() {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.open("POST", "/", true);
+                    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    xhttp.send(new FormData(form));
+                });
+            </script>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    myanchor.run(host="0.0.0.0")
