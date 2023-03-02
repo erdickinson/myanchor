882abc577f7715737d77f400c15d4ce92a3baaac
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-02 13:49:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import serial
 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 def send_to_arduino(value):
     checksum = sum(map(ord, value)) % 256
