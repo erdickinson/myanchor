2c40489002ae624bac3950a6c61f65db2a3d0c6c
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-03 14:18:22 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,34 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 import serial
-import time
 
+app = Flask(__name__)
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
 
-def send_to_arduino(value):
-    checksum = sum(map(ord, value)) % 256
-    message = "{}{:02X}\n".format(value, checksum)
-    ser.write(message.encode())
-    print("Sent value: {}".format(value))
-
-myanchor = Flask(__name__)
-
-@myanchor.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if request.form.get('speed') is not None:
-            speed = int(request.form.get('speed'))
-            send_to_arduino("S{:03d}".format(speed))
+        number = request.form['number']
+        ser.write(number.encode('utf-8'))
+        arduino_response = ser.read().decode('utf-8')
+        print("Received number from Arduino:", arduino_response)
+    return render_template('index.html')
 
-    # Counter functionality
-    counter = int(time.time() % 100)
-    send_to_arduino("C{:03d}".format(counter))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
     
-    return """
-    <html>
-        <body>
-            <h1>DC Motor Controller</h1>
-            <form id="motor-form" method="POST">
-                <label>Speed:</label>
-                <input type="range" min="0" max="255" value="0" class="slider" name="speed">
-            </form>
-            <div id="counter">Counter: {}</div>
-            <script>
-                var form = document.getElementById("motor-form");
-                var slider = document.querySelector(".slider");
-                form.addEventListener("input", function() {
-                    var xhttp = new XMLHttpRequest();
-                    xhttp.open("POST", "/", true);
-                    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                    xhttp.send(new FormData(form));
-                });
-
-                setInterval(function() {{
-                    var counterDiv = document.getElementById("counter");
-                    counterDiv.innerHTML = "Counter: " + parseInt(counterDiv.innerHTML.split(":")[1].trim()) + 1;
-                }}, 1000);
-            </script>
-        </body>
-    </html>
-    """.format(counter)
-
-
-if __name__ == "__main__":
-    myanchor.run(host="0.0.0.0")
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Send Number to Arduino</title>
+</head>
+<body>
+    <h1>Send Number to Arduino</h1>
+    <form method="POST">
+        <label for="number">Enter a number:</label>
+        <input type="text" id="number" name="number">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
